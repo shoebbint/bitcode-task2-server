34030b853d9f66831ed912c1042402f404f9d3ef
shoebbint <shoebbint@gmail.com>
2024-10-23 19:03:24 +0600

    issue-resolve

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const cors = require('cors'); 
 const PORT = process.env.PORT || 5000;
 const reportRoutes = require('./reports');
 const dataRoutes = require('./fetchData'); 
 const { sequelize } = require('../config/db');
-app.use(cors());
+
+// Configure CORS to allow requests from your frontend
+const corsOptions = {
+    origin: 'https://bitcode-frontend-task.vercel.app', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods if necessary
+    credentials: true // Allow credentials if you're using cookies or HTTP Auth
+};
+
+app.use(cors(corsOptions)); // Use the configured CORS options
 app.use(express.json());
 app.use('/api/reports', reportRoutes);
 app.use('/api', dataRoutes); 
