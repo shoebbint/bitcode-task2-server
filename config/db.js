f43aadb469043122a0ea1322f5bbeb3c2d5649d3
shoebbint <shoebbint@gmail.com>
2024-10-23 18:09:31 +0600

    live-done

--- config/db.js
+++ config/db.js
@@ -1,11 +1,19 @@
 // TIDBConnection.js
 const { Sequelize } = require('sequelize');
 require('dotenv').config(); // Load environment variables from .env file
+const fs = require('fs'); // Import file system module to read CA file
 
 const sequelize = new Sequelize(process.env.TIDB_NAME, process.env.TIDB_USER, process.env.TIDB_PASSWORD, {
   host: process.env.TIDB_HOST,
   dialect: 'mysql',
   port: process.env.TIDB_PORT,
+  dialectOptions: {
+    ssl: {
+      require: true, // This option is required for secure connections
+      rejectUnauthorized: true, // Validate server's certificate
+      ca: process.env.CA_PATH ? fs.readFileSync(process.env.CA_PATH) : undefined, // Read CA certificate from file if provided
+    },
+  },
 });
 
 async function testConnection() {
